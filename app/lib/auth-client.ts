cd9c1f0b56e1f888bea4a2e53b85492c00465123
hakmandul <ludmilagakman03@gmail.com>
2026-01-09 22:58:00 +0200

    commiit 3

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -1,10 +1,15 @@
 import { createAuthClient } from "better-auth/vue"
 
+// ОПРЕДЕЛЯЕМ БАЗОВЫЙ URL
+// import.meta.env.SSR — возвращает true, если код выполняется на сервере (Nuxt SSR)
+const baseURL = import.meta.env.SSR
+    ? "http://127.0.0.1:3001/api/auth" // SSR (сервер): стучимся локально и быстро
+    : "/api/auth";                     // Client (браузер): стучимся через Nginx (относительно)
+
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3001" // URL твоего Hono бэкенда
+    baseURL
 })
 
-// Экспортируем готовые хуки для удобства
 export const { 
     signIn, 
     signUp, 
